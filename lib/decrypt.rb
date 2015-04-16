e1165381c498bc3fd4d9118092f4097547557c8e
Chelsea Worrel <chelseaworrel@gmail.com>
2015-04-16 07:09:05 -0600

    fixed the decrypt.rb

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,12 +1,14 @@
 require './lib/decryptor'
 
-infile  = ARGV[0] || "encrypted.txt"
-outfile = ARGV[1] || "decrypted.txt"
+infile   = ARGV[0] || "encrypted.txt"
+outfile  = ARGV[1] || "decrypted.txt"
+key      = ARGV[2]
+date_key = ARGV[3]
 
-message = File.read(infile).split("\n").join
+message  = File.read(infile).split("\n").join
 
-key               = KeyGenerator.generate
-date_key          = DateKey.new.today
+# key               = KeyGenerator.generate
+# date_key          = DateKey.new.today
 decrypt           = Decryptor.new(key, date_key, message)
 decrypted_message = decrypt.return_encrypting_chars
 
